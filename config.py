a7504da729b65aaaabbb173a7feb72231f5e0e57
Nghia12a1ne <119744064+Nghia12a1ne@users.noreply.github.com>
2022-12-03 21:29:52 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-epochs = 1
-batch_size = 32
-model_path = "models/model.pth"
-char2int_path = "data/data/char2int.pkl"
-int2char_path = "data/data/int2char.pkl"
-data_file_path = "data/data/data_file.csv"
-image_path = "data/images"
-label_path = "data/labels.txt"
